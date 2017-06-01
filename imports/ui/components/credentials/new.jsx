3dff4107f493e9ffc337be9ddf394d5504e8dcc1
tylershuster <tyler@pacificsky.co>
2017-06-01 16:57:24 -0700

    Encrypting password on creation

--- imports/ui/components/credentials/new.jsx
+++ imports/ui/components/credentials/new.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Credential } from '/imports/api/credentials';
 import { handleError } from '/imports/ui/helpers';
+import CryptoJS from 'crypto-js';
 
 const CredentialNew = ({ onSubmit }) => (
 	<form
 		id="credentialNew"
 		onSubmit={event => {
 			event.preventDefault();
-			const credential = new Credential({
-				url: $(event.target).find('[name=url]').val(),
-				user: $(event.target).find('[name=user]').val() || undefined,
-				password: $(event.target).find('[name=password]').val()
-			});
-			Meteor.call('/credentials/create', credential, (error, credentialId) => {
-				handleError(error).then(() => {
-					onSubmit(credentialId);
+			if(localStorage.dashMasterKey) {
+				const credential = new Credential({
+					url: $(event.target).find('[name=url]').val(),
+					user: $(event.target).find('[name=user]').val() || undefined,
+					password: CryptoJs.AES.encrypt($(event.target).find('[name=password]').val(), localStorage.dashMasterKey).toString()
 				});
-			});
+				Meteor.call('/credentials/create', credential, (error, credentialId) => {
+					handleError(error).then(() => {
+						onSubmit(credentialId);
+					});
+				});
+			}
 		}}
 		>
 		<input required type="text" name="url" placeholder="url" />
